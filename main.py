dc96c76bbf0ac90a1fc140aee5c242d2db0ad3d7
Estime Jésus <estimejesus@gmail.com>
2022-11-16 08:07:21 +0200

    Change user credentiels for last commit and add multiplying function

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
-# age = 17
+""" age = 17
 
-# if age<18 :
-#     print("vous etes mineur")
-# elif age== 18 :
-#     print('vous etez un peu majeur ')
-# else :
-#     print("vous etez majeur")
+if age<18 :
+    print("vous etes mineur")
+elif age== 18 :
+    print('vous etez un peu majeur ')
+else :
+    print("vous etez majeur")
 
-# i=1
-# while i<10:
-#         print(i)
-#         i=i+1
-# else :
-#         print(i,"\' est plus egale a 10")
+i=1
+while i<10:
+        print(i)
+        i=i+1
+else :
+        print(i,"\' est plus egale a 10")
 
-# # Les listes
+# Les listes
 
-# fruits = ["Ananas" ,"Pomme", "Mangue","Avocat"]
+fruits = ["Ananas" ,"Pomme", "Mangue","Avocat"]
 
-# for f in fruits :
-#     print(f)  
-
-
-# # Les tuples
-
-# fruits = {"Ananas" ,"Pomme", "Mangue","Avocat"}
-
-# for f in fruits :
-#     print(f)
-
-# # Le sets
+for f in fruits :
+    print(f)  
 
 
-# fruits = {"Ananas" ,"Pomme", "Mangue","Avocat"}
+# Les tuples
 
-# for f in fruits :
-#     print(f)
+fruits = {"Ananas" ,"Pomme", "Mangue","Avocat"}
+
+for f in fruits :
+    print(f)
+
+# Le sets
 
 
-# Dictionnaires
+fruits = {"Ananas" ,"Pomme", "Mangue","Avocat"}
+
+for f in fruits :
+    print(f)
+
+
+Dictionnaires
  
 students = {
     "name": "John",
@@ -46,20 +46,20 @@ students = {
     "adress" : "Kamenge"
  }
 
-# students["age"]=18
+students["age"]=18
 
-# print(students)
-
-
-# for s in students.values():
-#     print(s)
+print(students)
 
 
-# del students
+for s in students.values():
+    print(s)
 
-# print(students)
 
-# Fonctions
+del students
+
+print(students)
+
+Fonctions
 
 
 def show_students(first_name,last_name):
@@ -68,5 +68,13 @@ def show_students(first_name,last_name):
 first_name=str(input("Veuiller saisir votre nom :")) 
 last_name=str(input("Veuiller saisir votre prénom :"))
 
-show_students(first_name,last_name)
+show_students(first_name,last_name) """
     
+
+def multiplying():
+    first_number=int(input("Veuiller saisir le 1er entier :")) 
+    second_number=int(input("Veuiller saisir le 2eme entier :"))
+    result=first_number*second_number
+    print("La multiplication de ",first_number,"et ",second_number," est ",result)
+
+multiplying()
